c6c194c526a5bc9bf1659702b930612f94973321
thouxanbanunotheactivist <priorityhighh@gmail.com>
2024-11-06 16:46:17 +0300

    initial

---
+++ protocol.py
@@ -0,0 +1,33 @@
+import socket
+
+
+class UDPBasedProtocol:
+    def __init__(self, *, local_addr, remote_addr):
+        self.udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.remote_addr = remote_addr
+        self.udp_socket.bind(local_addr)
+
+    def sendto(self, data):
+        return self.udp_socket.sendto(data, self.remote_addr)
+
+    def recvfrom(self, n):
+        msg, addr = self.udp_socket.recvfrom(n)
+        return msg
+
+    def close(self):
+        self.udp_socket.close()
+
+
+class MyTCPProtocol(UDPBasedProtocol):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def send(self, data: bytes):
+        return self.sendto(data)
+
+    def recv(self, n: int):
+        return self.recvfrom(n)
+    
+    def close(self):
+        super().close()
+
